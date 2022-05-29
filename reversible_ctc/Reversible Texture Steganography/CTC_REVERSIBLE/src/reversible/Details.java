defbc07f2aac7c03fb5a40c501faea513027f09f
Karthik Charan K <karthikcharan994@gmail.com>
2022-05-29 22:52:24 +0530

    Team_15 Project Code Committed

---
+++ reversible_ctc/Reversible Texture Steganography/CTC_REVERSIBLE/src/reversible/Details.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package reversible;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class Details 
+{
+    static int Tw = 488;
+    static int Th = 488; 
+    static int Pw = 48;
+    static int Ph = 48;
+    static int Pd = 8;
+    static int Tpw = 0;
+    static int Tph = 0;
+    static int TPn = 0;
+    static int Kw  = 32;
+    static int Kh = 32;
+    static int Sw = 0;
+    static int Sh = 0;
+    static int CPn=0;
+    static int Spn=0;
+    
+    static int blocki=0;
+    static int blockj=0;
+    
+    static double BPPmax=0;
+    
+    
+    static ArrayList<Integer> PatchID  =new ArrayList<Integer>();
+    
+}
